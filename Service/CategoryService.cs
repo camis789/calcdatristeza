dd5964dde0ecd2b0d88c1acdab6b69b38a77a2e7
Camila Caxambu <camila-caxambu@hotmail.com>
2019-11-16 17:48:25 -0300

    Service/

---
+++ Service/CategoryService.cs
@@ -0,0 +1,38 @@
+﻿using SisProdutos.Config;
+using SisProdutos.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SisProdutos.Service
+{
+    class CategoryService
+    {
+        DbContextProduct context = new DbContextProduct();
+
+        public Category AddCategory(Category category)
+        {
+            category.DateCreate = DateTime.Now;
+
+            context.Categorys.Add(category);
+
+
+            return category;
+        }
+
+        public bool CategoryTest(Category category)
+        {
+            var result = context.Categorys.Where(x => x.Name == category.Name).FirstOrDefault();
+            if (result != null)
+            {
+                return true;
+
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Service/ProductService.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using SisProdutos.models;
+using SisProdutos.Config;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace SisProdutos.Service
+{
+    public class ProductService
+    {
+
+        DbContextProduct context = new DbContextProduct();
+
+        public Product AddProduct(Product product)
+        {
+            product.DateCreate = DateTime.Now;
+
+            context.Products.Add(product);
+            context.SaveChanges();
+
+            return product;
+        }
+
+        public List<Product> ListProducts()
+        {
+            var result = context.Products.ToList();
+
+            return result;
+        }
+
+        public void verifica(string texto)
+        {
+            if (this.contemLetras(texto) && this.contemNumeros(texto))
+            {
+                //Contem Letras e Números
+            }
+            else if (this.contemLetras(texto))
+            {
+                //Contem somente letras
+            }
+            else if (this.contemNumeros(texto))
+            {
+                //Contem somente numeros
+            }
+        }
+
+        public bool contemLetras(string texto)
+        {
+            if (texto.Where(c => char.IsLetter(c)).Count() > 0)
+                return true;
+            else
+                return false;
+        }
+
+        public bool contemNumeros(string texto)
+        {
+            if (texto.Where(c => char.IsNumber(c)).Count() > 0)
+                return true;
+            else
+                return false;
+        }
+
+    }
+}
