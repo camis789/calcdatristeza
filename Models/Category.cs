21dea6f06df7e84219f81adcf1d57be456961fd1
Camila Caxambu <camila-caxambu@hotmail.com>
2019-11-16 17:47:53 -0300

    Models/

---
+++ Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace SisProdutos.models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+
+        public ICollection<Product> Products { get; set; }
+        public DateTime DateCreate { get; internal set; }
+
+
+    }
+
+}
